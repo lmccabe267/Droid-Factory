249edf3f68d103f76f43c44aee17e86e32926151
xaltt <samuelnunez@cpp.edu>
2021-09-24 14:47:05 -0700

    SourceCode

--- src/ArrayBagTest.java
+++ src/ArrayBagTest.java
@@ -1,4 +1,16 @@
 
-public class ArrayBagTest {
-
+public class ArrayBagTest extends ResizableArrayBag<String> {
+	
+	public static void main(String[]args) {
+		
+		ResizableArrayBag<String> newOne = new ResizableArrayBag<String>(2);
+		
+		
+		
+		System.out.println(newOne.getCurrentSize());
+		newOne.add("A");
+		System.out.println(newOne.getCurrentSize());
+	}
+	
+	
 }
