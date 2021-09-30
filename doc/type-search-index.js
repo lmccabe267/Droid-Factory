215d0487fcde464bebdd4eec8b28d1ac58b81543
Liam McCabe <lmccabe267@gmail.com>
2021-09-29 22:51:00 -0700

    updated javadocs

--- doc/type-search-index.js
+++ doc/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"LinkedBag"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"ArrayBagTest"},{"p":"<Unnamed>","l":"BagInterface"},{"p":"<Unnamed>","l":"LinkedBag"},{"p":"<Unnamed>","l":"LinkedBagTest"},{"p":"<Unnamed>","l":"ResizableArrayBag"}];updateSearchResults();
